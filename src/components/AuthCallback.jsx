a1436ea549d37c9bd96c1554fc0809646c055292
yasir5405 <yasirnaseem5415@gmail.com>
2025-03-02 02:13:38 +0530

    Try fixing auth in AuthCallback component by getting the user and dispatching it and then redirecting

--- src/components/AuthCallback.jsx
+++ src/components/AuthCallback.jsx
@@ -1,27 +1,29 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import authService from "../appwrite/auth";
+import { useDispatch } from "react-redux";
+import { login } from "../store/authSlice";
 
 const AuthCallback = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   useEffect(() => {
     async function completeLogin() {
       try {
-        const user = await authService.account.get();
+        const user = await authService.getCurrentUser();
         if (user) {
-          window.location.replace("/");
-        } else {
-          navigate("/login");
+          dispatch(login(user));
+          navigate("/"); // Redirect to home after login
         }
       } catch (error) {
-        console.error("OAuth Callback Error:", error);
-        window.location.replace("/login");
+        console.log("Error fetching user:", error);
+        navigate("/login"); // Redirect to login if it fails
       }
     }
 
     completeLogin();
-  }, [navigate]);
+  }, [navigate, dispatch]);
   return <div>Logging you in...</div>;
 };
 
