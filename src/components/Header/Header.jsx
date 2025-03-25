b5fc2a26a0da0fda5ab756679cbe77db02d93fa6
yasir5405 <yasirnaseem5415@gmail.com>
2025-03-25 20:44:37 +0530

    add hero image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,101 +1,3 @@
-// import { useSelector } from "react-redux";
-// import { Link, useNavigate } from "react-router-dom";
-// import authService from "../../appwrite/auth";
-// import { useDispatch } from "react-redux";
-// import { logout } from "../../store/authSlice";
-// import { NavLink } from "react-router-dom";
-
-// const Header = () => {
-//   const dispatch = useDispatch();
-//   const userData = useSelector((state) => state.auth.userData);
-
-//   const handleLogout = async () => {
-//     await authService.logout();
-//     dispatch(logout());
-//   };
-
-//   const navLinks = [
-//     {
-//       name: "Home",
-//       link: "/",
-//     },
-//     {
-//       name: "All Posts",
-//       link: "/all-posts",
-//     },
-//     {
-//       name: "Add Post",
-//       link: "/add-post",
-//     },
-//     {
-//       name: "Contact",
-//       link: "/contact",
-//     },
-//   ];
-
-//   return (
-//     <nav className="w-full h-[65px] flex items-center justify-between px-[150px] border-b-[1px] border-gray-200 shadow-md">
-//       <h1 className="text-[20px] font-[600] flex gap-[10px] items-center">
-//         <img src="https://stealthwriter.ai/logo-icon.svg" alt="" />
-//         BlogWala
-//       </h1>
-
-//       <div className="h-full items-center justify-center flex gap-[50px]">
-//         {navLinks.map(({ name, link }, i) => (
-//           <NavLink
-//             className={({ isActive }) =>
-//               `font-[500] text-[14px] hover:text-black transition-all ease-linear ${
-//                 isActive ? "text-black" : "text-zinc-500"
-//               }`
-//             }
-//             to={link}
-//             key={i}
-//           >
-//             {name}
-//           </NavLink>
-//         ))}
-//         {userData && (
-//           <NavLink
-//             className={({ isActive }) =>
-//               `font-[500] text-[14px] hover:text-black transition-all ease-linear ${
-//                 isActive ? "text-black" : "text-zinc-500"
-//               }`
-//             }
-//             to={`/${userData?.$id}`}
-//           >
-//             Profile
-//           </NavLink>
-//         )}
-//       </div>
-//       <div>
-//         {userData ? (
-//           <button
-//             className="font-medium text-[15px] border-[1px] border-zinc-400 py-[7px] px-[20px] text-[#FAFAFA] bg-[#2F2F31] rounded-lg transition-all ease-linear duration-100 hover:bg-[#3a3a3b]"
-//             onClick={handleLogout}
-//           >
-//             Logout
-//           </button>
-//         ) : (
-//           <div className="flex gap-[10px]">
-//             <Link to={"/login"}>
-//               <button className="font-[500] text-[15px] border-[1px] border-zinc-300 py-[7px] px-[20px] hover:bg-zinc-50 transition-all ease-linear duration-200 text-black rounded-lg">
-//                 Login
-//               </button>
-//             </Link>
-//             <Link to={"/signup"}>
-//               <button className="font-medium text-[15px] border-[1px] border-zinc-400 py-[7px] px-[20px] text-[#FAFAFA] bg-[#2F2F31] rounded-lg transition-all ease-linear duration-100 hover:bg-[#3a3a3b]">
-//                 Signup
-//               </button>
-//             </Link>
-//           </div>
-//         )}
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Header;
-
 import { useSelector, useDispatch } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
 import { useState, useEffect } from "react";
@@ -151,9 +53,9 @@ const Header = () => {
 
   return (
     <nav
-      className={`w-full h-[65px] flex items-center justify-between px-6 md:px-12 lg:px-20 border-b border-gray-600 shadow-md bg-zinc-950 text-white fixed top-0 left-0 z-50 ${
+      className={`w-full h-[65px] flex items-center justify-between px-6 md:px-12 lg:px-20 border-b border-gray-600 shadow-md text-white fixed top-0 left-0 z-50 ${
         isScrolled
-          ? "bg-white/5 backdrop-blur-lg border-b border-zinc-700 shadow-md"
+          ? "bg-white/5 backdrop-blur-md border-b border-zinc-700 shadow-lg"
           : "bg-zinc-950"
       }`}
     >
@@ -174,7 +76,7 @@ const Header = () => {
         {navLinks.map(({ name, link }, i) => (
           <NavLink
             className={({ isActive }) =>
-              `font-normal text-sm hover:text-zinc-300 transition-all ease-linear ${
+              `font-medium text-sm hover:text-zinc-300 transition-all ease-linear ${
                 isActive ? "text-zinc-200" : "text-[#B5B5B7]"
               }`
             }
